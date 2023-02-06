d70851a9bb390b5d3963774d0b446c02e254e51b
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-02-06 16:19:35 +0100

    Fix

--- poseidon-permutation/src/lib.rs
+++ poseidon-permutation/src/lib.rs
@@ -1,7 +1,6 @@
 //! An implemention of the Poseidon permutation for fixed-width
 //! hashing.
-// #![cfg_attr(not(feature = "std"), no_std)]
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 mod permutation;
 
